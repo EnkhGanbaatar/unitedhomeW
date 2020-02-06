6de3baf38ac392e01305b5745e125f21a064a78d
Enkhmend Ganbaatar <mendee@Enkhmends-MBP.fios-router.home>
2020-02-06 18:44:43 -0500

    united home w

---
+++ src/main/java/javaBasic/LearningJavaVariable.java
@@ -0,0 +1,77 @@
+package javaBasic;
+
+public class LearningJavaVariable {
+
+	public static void main(String[] args) {
+	//*
+	
+		String make= "Tesla";
+		String model= "ModelX";
+		
+		
+		
+		System.out.println("My next year new car will be "+make + " "+model);
+
+		
+		String salary_$;
+		
+		
+		String my1stGoal;
+		
+		
+		int $;
+		
+		String a23498234982u4_$;
+		
+		String $324525235;
+		
+		
+		int _Ok;
+		
+String victoriaOfMendee;
+
+
+
+       byte byteValue = -128;
+       
+       long longvalue = 12817299999L;
+       //
+       
+       char[] acharArray = {'\u0057','\u0065','\u006c',
+				'\u0063','\u006f','\u006d','\u0065',
+				'\u0074','\u006f','\u0074','\u0065',
+				'\u0063','\u0068','\u0043','\u0069',
+				'\u0072','\u0063','\u006c','\u0065'};
+		System.out.println(acharArray);
+       
+       float a = 9.9999f;
+       
+       int b = (int) 9.9f;
+    		   
+    		   
+      // System.out.println("a");
+       
+       
+		
+		
+		byte byteNumber = -127;
+		
+		
+		short shourtValue = 325;
+		
+		byte newbyteValue = (byte) shourtValue;
+		
+		
+		System.out.println("shortValue");
+		
+		System.out.println("newShourtValue");
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/main/java/testing/packages/Techcircle.java
@@ -0,0 +1,15 @@
+package testing.packages;
+
+public class Techcircle {
+
+	public static void main(String[] args) {
+		System.out.println("Techcircle");
+
+		
+   
+		
+		
+		
+	}
+
+}
